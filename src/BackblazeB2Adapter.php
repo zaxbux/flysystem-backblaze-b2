f85ed75b28ace7977b053c6dbbdb415cc9b27d50
Zachary Schneider <hello@zacharyschneider.ca>
2019-03-24 18:27:44 -0700

    Initial commit

---
+++ src/BackblazeB2Adapter.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Zaxbux\Flysystem;
+
+use ILAB\B2\Client;
+use ILAB\B2\Bucket;
+use GuzzleHttp\Psr7;
+use League\Flysystem\Config;
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\Adapter\Polyfill\NotSupportingVisibilityTrait;
+
+class BackblazeB2Adapter extends AbstractAdapter {
+	use NotSupportingVisibilityTrait;
+
+	protected $client;
+	protected $bucketName;
+
+	public function __construct(Client $client, $bucketName) {
+		$this->client = $client;
+		$this->bucketName = $bucketName;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function has($path) {
+		return $this->getClient()->fileExists([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $path,
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function write($path, $contents, Config $config) {
+		return $this->writeStream($path, $contents, $config);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function writeStream($path, $resource, Config $config) {
+		$file = $this->getClient()->upload([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $path,
+			'Body'       => $resource,
+		]);
+
+		return $this->getFileInfo($file);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function update($path, $contents, Config $config) {
+		return $this->writeStream($path, $contents, $config);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function updateStream($path, $resource, Config $config) {
+		return $this->writeStream($path, $resource, $config);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function read($path) {
+		$file = $this->getClient()->getFile([
+			'BucketName' => $this->bucketName(),
+			'FileName'   => $path
+		]);
+
+		$fileContents = $this->getClient()->download([
+			'FileId' => $file->getId(),
+		]);
+
+		return ['contents' => $fileContents];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function readStream($path) {
+		$stream   = Psr7\stream_for();
+		$download = $this->getClient()->download([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $path,
+			'SaveAs'     => $stream,
+		]);
+
+		$stream->seek(0);
+
+		try {
+			$resource = Psr7\StreamWrapper::getResource($stream);
+		} catch (InvalidArgumentException $e) {
+			return false;
+		}
+
+		return $download === true ? ['stream' => $resource] : false;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rename($path, $newPath) {
+		// The B2 API does not support re-naming
+		return false;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function copy($path, $newPath) {
+		return $this->getClient()->upload([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $newPath,
+			'Body'       => @\file_get_contents($path),
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function delete($path) {
+		return $this->getClient()->deleteFile([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $path
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function deleteDir($path) {
+		return $this->delete($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function createDir($path, Config $config) {
+		return $this->write($path, '', $config);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getMetadata($path) {
+		$file = $this->getClient()->getFile([
+			'BucketName' => $this->bucketName,
+			'FileName'   => $path,
+		]);
+
+		return $this->getFileInfo($file);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getMimetype($path) {
+		return $this->getMetadata($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getSize($path) {
+		return $this->getMetadata($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTimestamp($path) {
+		return $this->getMetadata($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getClient() {
+		return $this->client;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function listContents($directory = '', $recursive = false) {
+		$files = $this->getClient()->listFiles([
+			'BucketName' => $this->bucketName,
+			'delimiter'  => '/',
+			'prefix'     => $directory
+		]);
+
+		var_dump($files);
+
+		return $files;
+	}
+
+	/**
+	 * Get file info
+	 * 
+	 * @param  ILAB\B2\File $file
+	 * @return array
+	 */
+	protected function getFileInfo($file) {
+		return [
+			'type'      => $file->getType(),
+			'path'      => $file->getName(),
+			'timestamp' => $file->getUploadTimestamp(),
+			'size'      => $file->getSize()
+		];
+	}
+}
