5ceba826daaf6f5997e2a2e622e9a0042b93cf1e
Zachary Schneider <hello@zacharyschneider.ca>
2022-10-19 15:11:20 -0700

    tests

--- spec/UnitTest.php
+++ spec/UnitTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use League\Flysystem\ChecksumAlgoIsNotSupported;
+use League\Flysystem\Config;
 use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\UnableToDeleteFile;
 use League\Flysystem\UnableToRetrieveMetadata;
+use League\Flysystem\UrlGeneration\PublicUrlGenerator;
 use Zaxbux\BackblazeB2\Client;
 use Zaxbux\Flysystem\BackblazeB2Adapter;
 
@@ -25,6 +28,7 @@ class UnitTest extends \League\Flysystem\AdapterTestUtilities\FilesystemAdapterT
     public function overwriting_a_file(): void
     {
         $this->expectException(UnableToRetrieveMetadata::class);
+        $this->expectExceptionMessage('object-level ACLs are not supported');
 
         parent::overwriting_a_file();
     }
@@ -35,6 +39,7 @@ class UnitTest extends \League\Flysystem\AdapterTestUtilities\FilesystemAdapterT
     public function setting_visibility(): void
     {
         $this->expectException(UnableToRetrieveMetadata::class);
+        $this->expectExceptionMessage('object-level ACLs are not supported');
 
         parent::setting_visibility();
     }
@@ -44,9 +49,21 @@ class UnitTest extends \League\Flysystem\AdapterTestUtilities\FilesystemAdapterT
      */
     public function copying_a_file(): void
     {
-        $this->expectException(UnableToRetrieveMetadata::class);
+        $this->runScenario(function () {
+            $adapter = $this->adapter();
+            $adapter->write(
+                'source.txt',
+                'contents to be copied',
+                new Config([])
+            );
 
-        parent::copying_a_file();
+            $adapter->copy('source.txt', 'destination.txt', new Config());
+
+            $this->assertTrue($adapter->fileExists('source.txt'));
+            $this->assertTrue($adapter->fileExists('destination.txt'));
+            //$this->assertEquals(Visibility::PUBLIC, $adapter->visibility('destination.txt')->visibility());
+            $this->assertEquals('contents to be copied', $adapter->read('destination.txt'));
+        });
     }
 
     /**
@@ -54,9 +71,21 @@ class UnitTest extends \League\Flysystem\AdapterTestUtilities\FilesystemAdapterT
      */
     public function copying_a_file_again(): void
     {
-        $this->expectException(UnableToRetrieveMetadata::class);
+        $this->runScenario(function () {
+            $adapter = $this->adapter();
+            $adapter->write(
+                'source.txt',
+                'contents to be copied',
+                new Config([])
+            );
 
-        parent::copying_a_file_again();
+            $adapter->copy('source.txt', 'destination.txt', new Config());
+
+            $this->assertTrue($adapter->fileExists('source.txt'));
+            $this->assertTrue($adapter->fileExists('destination.txt'));
+            //$this->assertEquals(Visibility::PUBLIC, $adapter->visibility('destination.txt')->visibility());
+            $this->assertEquals('contents to be copied', $adapter->read('destination.txt'));
+        });
     }
 
     /**
@@ -64,18 +93,88 @@ class UnitTest extends \League\Flysystem\AdapterTestUtilities\FilesystemAdapterT
      */
     public function moving_a_file(): void
     {
-        $this->expectException(UnableToRetrieveMetadata::class);
-
-        parent::moving_a_file();
+        $this->runScenario(function () {
+            $adapter = $this->adapter();
+            $adapter->write(
+                'source.txt',
+                'contents to be copied',
+                new Config([/* Config::OPTION_VISIBILITY => Visibility::PUBLIC */])
+            );
+            $adapter->move('source.txt', 'destination.txt', new Config());
+            $this->assertFalse(
+                $adapter->fileExists('source.txt'),
+                'After moving a file should no longer exist in the original location.'
+            );
+            $this->assertTrue(
+                $adapter->fileExists('destination.txt'),
+                'After moving, a file should be present at the new location.'
+            );
+            /* $this->assertEquals(Visibility::PUBLIC, $adapter->visibility('destination.txt')->visibility()); */
+            $this->assertEquals('contents to be copied', $adapter->read('destination.txt'));
+        });
     }
 
     /**
      * @test
      */
-    public function trying_to_delete_a_non_existing_file(): void
+    public function fetching_last_modified(): void
     {
-        $this->expectException(UnableToDeleteFile::class);
+        // This exception is expected since the last modified timestamp must be explicitly set when uploading a file.
+        $this->expectException(UnableToRetrieveMetadata::class);
 
-        parent::trying_to_delete_a_non_existing_file();
+        parent::fetching_last_modified();
+    }
+
+    /**
+     * @test
+     */
+    public function generating_a_public_url(): void
+    {
+        /** @var BackblazeB2Adapter $adapter */
+        $adapter = $this->adapter();
+
+        $adapter->write('some/path.txt', 'public contents', new Config(/* ['visibility' => 'public'] */));
+
+        $url = $adapter->publicUrl('some/path.txt', new Config([
+            'valid_duration' => 10,
+        ]));
+        $contents = file_get_contents($url);
+
+        self::assertEquals('public contents', $contents);
+    }
+
+    /**
+     * @test
+     */
+    public function expired_download_authorization(): void
+    {
+        /** @var BackblazeB2Adapter $adapter */
+        $adapter = $this->adapter();
+
+        $adapter->write('some/path.txt', 'public contents', new Config());
+
+        $url = $adapter->publicUrl('some/path.txt', new Config([
+            'valid_duration' => 5,
+        ]));
+
+        // Wait until after authorization expires
+        sleep(6);
+
+        self::assertFalse(@file_get_contents($url));
+    }
+
+    /**
+     * @test
+     */
+    public function specifying_a_custom_checksum_algo_is_not_supported(): void
+    {
+        /** @var BackblazeB2Adapter $adapter */
+        $adapter = $this->adapter();
+
+        $adapter->write('some/path.txt', 'public contents', new Config());
+
+        $this->expectException(ChecksumAlgoIsNotSupported::class);
+
+        $adapter->checksum('some/path.txt', new Config(['checksum_algo' => 'crc']));
     }
 }
